bf44e672370bd2d893e24cd707d441e797397b65
Mark Gaiser <markg85@gmail.com>
2021-04-10 14:58:36 +0200

    Provide a little more information to the user about the connection, Also: - When the CPU is above 70%, tell the user to use a different server. A connection is still OK. - When the CPU usgae is above 90%, kill the connection. The user will be informed about it but that's all before the connection is killed.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
+const osu = require('node-os-utils')
 const io = require('socket.io')(fastify.server, {
     cors: {
         origin: "*",
@@ -21,9 +22,58 @@ fastify.get('/', async (request, reply) => {
     return JSON.stringify(await ipfs.pubsub.ls())
 })
 
-io.on('connection', (socket) => {
+async function welcomeConnection(socket) {
+    let usage = await osu.cpu.usage();
+    let status = "CONN_OK"
+    let message = "You are now connected."
+
+    console.log(`CPU USAGE: ${usage}`)
+
+    /*
+        We give the user different status messages.
+
+        CONN_OK
+          The connection is OK. The user can do whatever.
+
+        CONN_HIGH_LOAD
+          If the CPU usage is above 70% this status is given.
+          A connection is still accepted but not preferred.
+
+        CONN_KILLED_TOO_HIGH_CPU_LOAD
+          If the CPU usahe gets above 90%, all new connections are
+          given this status and killed immediately after.
+          The user must connect to a different server.
+    */
+    if (usage > 70.0 && usage <= 90.0) {
+        console.log(`PLEASE USE ANOTHER SERVER`)
+        status = "CONN_HIGH_LOAD"
+        message = "Please consider using another server. This one has a high CPU load. (>70%)"
+    } else if (usage > 90.0) {
+        console.log(`KICKING USER, too high CPU load`)
+        status = "CONN_KILLED_TOO_HIGH_CPU_LOAD"
+        message = "The CPU load is above 90%. Not accepting new connections, closing. Use a different server."
+    }
+
+    return {
+        id: socket.id,
+        status: status,
+        message: message
+    }
+}
+
+io.on('connection', async (socket) => {
     console.log(`Connected. Socket id: ${socket.id}`)
-    socket.emit(`info`, socket.id)
+    let welcomePackage = await welcomeConnection(socket)
+    let stringData = JSON.stringify(welcomePackage)
+    let buffer = Buffer.from(stringData).toString('base64')
+    socket.emit(`info`, buffer)
+
+    if (welcomePackage.status == "CONN_KILLED_TOO_HIGH_CPU_LOAD") {
+        socket.disconnect();
+        console.log(`Forcefully disconnected ${socket.id} due to high CPU load.`)
+        return;
+    }
+
     socket.on('message', (data) => {
         console.log(data)
     });
